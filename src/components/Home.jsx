b275cb4eeb9aa9a5ea327b4b1581401181229526
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-09-25 16:27:59 -0300

    learn redux

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-zinc-800 text-2xl font-bold">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
